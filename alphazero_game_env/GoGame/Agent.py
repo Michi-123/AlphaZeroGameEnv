f6aa01abcfda4fa0a8ad3d333a80d2ad2781f5be
Michi-123 <hairstyle2004@yahoo.co.jp>
2024-06-30 01:51:09 +0900

    bug fix

--- alphazero_game_env/GoGame/Agent.py
+++ alphazero_game_env/GoGame/Agent.py
@@ -11,12 +11,25 @@ class Agent(Agent):
     def human(self, node, env):
         state = node.states[0]
         self.util.show_legal_actions(self.env)
+        is_pass = False
 
         while True:
             x = input('横,縦')
+            
+            if x == 'pass' or x == 'p':
+                is_pass = True
+                break
+
+            x = x.replace(',',' ')
+
             try:
-                x1, x2 = x.split(',')
+                x1, x2 = x.split()
                 x1, x2 = int(x1), int(x2)
+                
+                if x1 == -1:
+                    is_pass = True
+                    break
+                
                 if state[x2][x1] == 0:
                     break
                 else:
@@ -25,8 +38,7 @@ class Agent(Agent):
             except:
                 pass
 
-        if x1 == -1:
-            # action = self.CFG.action_size
+        if is_pass:
             action = self.CFG.pass_action
         else:
             # 縦横入れ替え
