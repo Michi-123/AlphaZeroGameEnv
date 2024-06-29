bb537f8d6181aa295667676069c1da2176c3989b
Michi-123 <hairstyle2004@yahoo.co.jp>
2024-06-29 18:08:53 +0900

    added alphazero modules

--- alphazero_game_env/GoGame/__init__.py
+++ alphazero_game_env/GoGame/__init__.py
@@ -3,3 +3,8 @@ sys.path.append('./')
 
 from .GoGame import GoGame
 from .SimpleGame import SimpleGame
+from .AlphaZeroNetwork import AlphaZeroNetwork
+from .Agent import Agent
+from .MCTS import MCTS
+from .SemiSelfPlay import SemiSelfPlay
+
